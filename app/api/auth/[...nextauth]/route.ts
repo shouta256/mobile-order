32ab3ddc658f9fabcb7e601c6c5b49f3de8736e1
shota <shouta160618@gmail.com>
2025-05-21 09:49:54 -0500

    ランタイム指定を追加

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,9 @@
 // app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth/next";
+
+export const runtime = "nodejs";
+
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
